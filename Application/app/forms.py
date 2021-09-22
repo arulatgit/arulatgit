0b161ca11fbed93f31842dd4603d844089d615a8
arul narayanan <arul4narayanan@gmail.com>
2021-09-22 23:26:52 +0530

    Update forms.py

--- Application/app/forms.py
+++ Application/app/forms.py
@@ -16,3 +16,18 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
+
